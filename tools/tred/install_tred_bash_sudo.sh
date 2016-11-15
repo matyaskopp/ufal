90771277666be272f5fa48134e2f10d3dc7cfb9e
Matyáš Kopp <matyas.kopp@gmail.com>
2016-11-15 10:42:40 +0100

    added sudo tred installation script

---
+++ tools/tred/install_tred_bash_sudo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TESTLOCATION='http://ufal.mff.cuni.cz/~kopp/tred_beta'
+cd ~/Downloads
+sudo apt-get install libx11-dev libxft-dev libfontconfig1-dev libpng12-dev zlib1g-dev libxml2-dev
+
+wget "$TESTLOCATION/install_tred.bash"
+sed -i .bak "s@http://ufal.mff.cuni.cz/tred@$TESTLOCATION@" install_tred.bash
+
+
+echo "Installing TrEd"
+sudo bash install_tred.bash --tred-dir ~/tred
