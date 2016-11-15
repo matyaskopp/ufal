1118db64f6fc5f8541244e927cc5cda95f4bf493
Matyáš Kopp <matyas.kopp@gmail.com>
2016-11-15 13:10:33 +0100

    fix file location

--- tools/tred/install_tred_deb.sh
+++ tools/tred/install_tred_deb.sh
@@ -3,7 +3,7 @@ TESTLOCATION='http://ufal.mff.cuni.cz/~kopp/tred_beta'
 cd ~/Downloads
 sudo apt-get install libx11-dev libxft-dev libfontconfig1-dev libpng12-dev zlib1g-dev libxml2-dev
 
-wget "$TESTLOCATION/install_tred.deb"
+wget "$TESTLOCATION/tred.deb"
 
 
 echo "Installing TrEd"
