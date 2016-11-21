c85405320df4305d065f63d60f66e1216af1fa6e
Matyáš Kopp <matyas.kopp@gmail.com>
2016-11-21 15:30:05 +0100

    cpan initialization, install some modules

--- tools/tred/install_tred_bash_usermode.sh
+++ tools/tred/install_tred_bash_usermode.sh
@@ -6,10 +6,28 @@ sudo apt-get install libx11-dev libxft-dev libfontconfig1-dev libpng12-dev zlib1
 wget "$TESTLOCATION/install_tred.bash"
 sed -i "s@http://ufal.mff.cuni.cz/tred@$TESTLOCATION@" install_tred.bash
 
-echo "Setting CPAN"
-#echo `eval $(perl -I$HOME/.perl/lib/perl5 -Mlocal::lib=$HOME/.perl)` >> ~/.bashrc
-#perl -I$HOME/.perl/lib/perl5 -Mlocal::lib=$HOME/.perl
-cpan
+#Setting up cpan (so that it uses local directories; http://www.perlmonks.org/?node_id=630026):
+mkdir -p ~/.cpan/CPAN 
+touch ~/.cpan/CPAN/MyConfig.pm (or echo "1" >~/.cpan/CPAN/MyConfig.pm, or cp /root/.cpan/CPAN/Config.pm ~/.cpan/CPAN/MyConfig.pm if the subsequent command does not work)
+
+echo "o conf init\nuse local::lib\nadd settings to .bashrc\npress ctrl+d to exit"
+perl -MCPAN -e shell 
+
+#Installing cpanm for easier installation of other Perl modules
+cpan App::cpanminus
+
+#Installing some dependencies (UNIVERSAL::DOES, patched PerlIO::gzip, Treex::PML)
+cpanm UNIVERSAL::DOES
+wget http://search.cpan.org/CPAN/authors/id/N/NW/NWCLARK/PerlIO-gzip-0.18.tar.gz
+wget http://www.cpan.org/authors/id/S/SR/SREZIC/patches/PerlIO-gzip-0.18-RT92412.patch
+tar -xzf PerlIO-gzip-0.18.tar.gz 
+patch PerlIO-gzip-0.18/Makefile.PL < PerlIO-gzip-0.18-RT92412.patch 
+cd PerlIO-gzip-0.18/ 
+perl Makefile.PL 
+make && make test && make install 
+cd .. 
+rm -rf PerlIO-gzip-0.18*
+cpanm Treex::PML
 
 
 
