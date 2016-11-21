dd8068759f7265ee78efe0f017cedffeeca0f1a6
Matyáš Kopp <matyas.kopp@gmail.com>
2016-11-21 15:53:45 +0100

    fixing CPAN/MyConfig.pm

--- tools/tred/install_tred_bash_usermode.sh
+++ tools/tred/install_tred_bash_usermode.sh
@@ -8,7 +8,8 @@ sed -i "s@http://ufal.mff.cuni.cz/tred@$TESTLOCATION@" install_tred.bash
 
 #Setting up cpan (so that it uses local directories; http://www.perlmonks.org/?node_id=630026):
 mkdir -p ~/.cpan/CPAN 
-touch ~/.cpan/CPAN/MyConfig.pm #(or echo "1" >~/.cpan/CPAN/MyConfig.pm, or cp /root/.cpan/CPAN/Config.pm ~/.cpan/CPAN/MyConfig.pm if the subsequent command does not work)
+#touch ~/.cpan/CPAN/MyConfig.pm #(or echo "1" >~/.cpan/CPAN/MyConfig.pm, or cp /root/.cpan/CPAN/Config.pm ~/.cpan/CPAN/MyConfig.pm if the subsequent command does not work)
+echo "1" >~/.cpan/CPAN/MyConfig.pm
 
 echo "o conf init\nuse local::lib\nadd settings to .bashrc\npress ctrl+d to exit"
 perl -MCPAN -e shell 
