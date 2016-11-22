bd645268685a0a987151edb81f775edda8b5893a
Matyáš Kopp <matyas.kopp@gmail.com>
2016-11-22 10:40:14 +0100

    remove installing deps add manpath

--- tools/tred/install_tred_bash_usermode.sh
+++ tools/tred/install_tred_bash_usermode.sh
@@ -9,21 +9,24 @@ sed -i "s@http://ufal.mff.cuni.cz/tred@$TESTLOCATION@" install_tred.bash
 
 wget -O- http://cpanmin.us | perl - -l ~/perl5 App::cpanminus local::lib || exit 2
 eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`
+eval `export MANPATH=$HOME/perl5/man:$MANPATH`
 echo 'eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`' >> ~/.profile
 echo 'export MANPATH=$HOME/perl5/man:$MANPATH' >> ~/.profile
 
 #Installing some dependencies (UNIVERSAL::DOES, patched PerlIO::gzip, Treex::PML)
-cpanm UNIVERSAL::DOES
-wget http://search.cpan.org/CPAN/authors/id/N/NW/NWCLARK/PerlIO-gzip-0.18.tar.gz
-wget http://www.cpan.org/authors/id/S/SR/SREZIC/patches/PerlIO-gzip-0.18-RT92412.patch
-tar -xzf PerlIO-gzip-0.18.tar.gz 
-patch PerlIO-gzip-0.18/Makefile.PL < PerlIO-gzip-0.18-RT92412.patch 
-cd PerlIO-gzip-0.18/ 
-perl Makefile.PL 
-make && make test && make install 
-cd .. 
-rm -rf PerlIO-gzip-0.18*
-cpanm Treex::PML
+# cpanm UNIVERSAL::DOES - not needed
+
+#wget http://search.cpan.org/CPAN/authors/id/N/NW/NWCLARK/PerlIO-gzip-0.18.tar.gz
+#wget http://www.cpan.org/authors/id/S/SR/SREZIC/patches/PerlIO-gzip-0.18-RT92412.patch
+#tar -xzf PerlIO-gzip-0.18.tar.gz 
+#patch PerlIO-gzip-0.18/Makefile.PL < PerlIO-gzip-0.18-RT92412.patch 
+#cd PerlIO-gzip-0.18/ 
+#perl Makefile.PL 
+#make && make test && make install 
+#cd .. 
+#rm -rf PerlIO-gzip-0.18*
+
+#cpanm Treex::PML
 
 
 
